f4aaeb6f6a576b45f6c2a41d360caf0aa05fe054
Abhishek kumar <89969964+Abhishek-kumar-202063@users.noreply.github.com>
2024-12-23 01:23:01 +0530

    Update gsp297.sh

--- gsp297.sh
+++ gsp297.sh
@@ -1,31 +1,9 @@
 
-
-
-BLACK=`tput setaf 0`
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-WHITE=`tput setaf 7`
-
-BG_BLACK=`tput setab 0`
-BG_RED=`tput setab 1`
-BG_GREEN=`tput setab 2`
-BG_YELLOW=`tput setab 3`
-BG_BLUE=`tput setab 4`
-BG_MAGENTA=`tput setab 5`
-BG_CYAN=`tput setab 6`
-BG_WHITE=`tput setab 7`
-
-BOLD=`tput bold`
-RESET=`tput sgr0`
 #----------------------------------------------------start--------------------------------------------------#
 cd ~
-curl -LO https://raw.githubusercontent.com/Abhishek-kumar-202063/Cloud-Hustlers-Creativity/refs/heads/main/subscribe.sh
-chmod +x subscribe.sh
-./subscribe.sh
+curl -LO https://raw.githubusercontent.com/Abhishek-kumar-202063/Cloud-Hustlers-Creativity/refs/heads/main/test.sh
+chmod +x test.sh
+./test.sh
 
 
 
